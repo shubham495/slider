f813cde6a857066a1f33a359db16dd1e128de011
shubham495 <123830897+shubham495@users.noreply.github.com>
2023-07-02 02:06:40 -0700

    Add files via upload
    carousel slider by swiper

---
+++ Shubhamwad.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper";
+
+
+const Shubhamwad = () => {
+  return (
+    <div>
+     
+      
+     <Swiper
+                navigation={true}
+                modules={[Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <img
+                        className="object-fill w-full h-96"
+                        src="https://cdn.pixabay.com/photo/2022/03/20/15/40/nature-7081138__340.jpg"
+                        alt="image slide 1"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="object-fill w-full h-96"
+                        src="https://cdn.pixabay.com/photo/2022/07/24/17/55/wind-energy-7342177__340.jpg"
+                        alt="image slide 2"
+                    />
+                     </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        className="object-fill w-full h-96"
+                        src="https://cdn.pixabay.com/photo/2022/07/26/03/35/jogger-7344979__340.jpg"
+                        alt="image slide 3"
+                    />
+                </SwiperSlide>
+            </Swiper>
+ 
+
+    </div>
+  )
+}
+
+export default Shubhamwad
